cd0b18c14fa30abbb7b42a3599364c578749ae29
trust0212 <zaza.liveedu@gmail.com>
2023-06-15 17:56:42 -0400

    complete

--- config.py
+++ config.py
@@ -1,9 +1,13 @@
 import os
+from web3 import Web3
 from dotenv import load_dotenv
 load_dotenv()
 
 PRIVATE_KEY = os.getenv('PRIVATE_KEY')
 POOL_ADDRESS = '0xD446eb1660F766d533BeCeEf890Df7A69d26f7d1' # AVAX-USDC pool
 TARGET_BIN_OFFSET = 2
-MIN_TOKEN_X_AMOUNT = 0.0001
-MIN_TOKEN_Y_AMOUNT = 0.01
+
+MAX_TOKEN_X_AMOUNT = Web3.to_wei(100, 'ether') # 100 AVAX
+MAX_TOKEN_Y_AMOUNT = 100_000_000 # 100 USDC
+
+MIN_BIN_COUNT = 10
--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 from web3 import Web3
 from constants import INFURA_URL, ROUTER_ADDRESS
-from config import POOL_ADDRESS, PRIVATE_KEY, TARGET_BIN_OFFSET, MIN_TOKEN_X_AMOUNT, MIN_TOKEN_Y_AMOUNT
+from config import POOL_ADDRESS, PRIVATE_KEY, TARGET_BIN_OFFSET, MAX_TOKEN_X_AMOUNT, MAX_TOKEN_Y_AMOUNT, MIN_BIN_COUNT
 from abi.LBRouter import ROUTER_ABI
 from abi.LBPair import PAIR_ABI
 from abi.ERC20 import ERC20_ABI
@@ -73,9 +73,8 @@ def job():
     binData = []
     bin_ids = []
     for id_data in binData_raw:
-        if id_data['tokenX']['amount'] >= MIN_TOKEN_X_AMOUNT or id_data['tokenY']['amount'] >= MIN_TOKEN_Y_AMOUNT:
-            bin_ids.append(id_data['binId'])
-            binData.append(id_data)
+        bin_ids.append(id_data['binId'])
+        binData.append(id_data)
     
     target_bin_id_X = bin_ids[TARGET_BIN_OFFSET - 1]
     target_bin_id_Y = bin_ids[len(bin_ids) - TARGET_BIN_OFFSET]
@@ -85,22 +84,28 @@ def job():
     print(target_bin_id_X)
     print(target_bin_id_Y)
 
+    if (len(bin_ids) < MIN_BIN_COUNT):
+        print("Bin length is insufficient")
+        return
+
     if active_bin <= target_bin_id_X or active_bin >= target_bin_id_Y:
         swapRoute = []
         swapToken = tokenX
         deleted_data = binData[-1 * TARGET_BIN_OFFSET:]
-
+        maxValue = MAX_TOKEN_X_AMOUNT
         if active_bin == target_bin_id_X:
             deleted_data = binData[-1 * TARGET_BIN_OFFSET:]
             swapToken = tokenX
+            maxValue = MAX_TOKEN_X_AMOUNT
             swapRoute = [tokenXAddress, tokenYAddress]
         else:
             deleted_data = binData[0: TARGET_BIN_OFFSET]
             swapToken = tokenY
+            maxValue = MAX_TOKEN_Y_AMOUNT
             swapRoute = [tokenYAddress, tokenXAddress]
         removeLiquidity(deleted_data)
         time.sleep(10)
-        swap(swapToken, swapRoute)
+        swap(swapToken, swapRoute, maxValue)
         time.sleep(10)
         addLiquidity(active_bin, active_bin <= target_bin_id_X)
         time.sleep(10)
@@ -110,8 +115,9 @@ def removeLiquidity(bin_data):
     amounts = []
     ids = []
     for bin in bin_data:
-        amounts.append(int(bin['liquidity']))
-        ids.append(int(bin['binId']))
+        id = int(bin['binId'])
+        amounts.append(pool.balanceof(account.address, id))
+        ids.append(id)
     deadline = web3.eth.get_block('latest').timestamp + 60
     nonce = web3.eth.get_transaction_count(account.address)
     call_function = router.functions.removeLiquidity(tokenXAddress, tokenYAddress, bin_step, 0, 0, ids, amounts, account.address, deadline).build_transaction({"chainId": chain_id, "from": account.address, "nonce": nonce})
@@ -121,7 +127,7 @@ def removeLiquidity(bin_data):
     tx_receipt = web3.eth.wait_for_transaction_receipt(send_tx)
     print(tx_receipt) # Optional
 
-def swap(swapToken, route):
+def swap(swapToken, route, maxValue):
     print("Swap")
     path = {
         "pairBinSteps": [bin_step],
@@ -130,7 +136,12 @@ def swap(swapToken, route):
     }
     nonce = web3.eth.get_transaction_count(account.address)
     deadline = web3.eth.get_block('latest').timestamp + 60
-    amountIn = math.floor(swapToken.functions.balanceOf(account.address).call() / 2)
+    amountIn = min(math.floor(swapToken.functions.balanceOf(account.address).call() / 2), maxValue)
+    print(path)
+    print(amountIn)
+    if amountIn <= 100:
+        print("Insufficient Amount")
+        return
     # Swap half of token
     call_function = router.functions.swapExactTokensForTokens(amountIn, 0, path, account.address, deadline).build_transaction({"chainId": chain_id, "from": account.address, "nonce": nonce})
     
@@ -146,8 +157,8 @@ def addLiquidity(active_bin, isX):
     idSlippage = getIdSlippageFromPriceSlippage(0.5, bin_step)
     nonce = web3.eth.get_transaction_count(account.address)
     deadline = web3.eth.get_block('latest').timestamp + 60
-    amountX = tokenX.functions.balanceOf(account.address).call()
-    amountY = tokenY.functions.balanceOf(account.address).call()
+    amountX = min(tokenX.functions.balanceOf(account.address).call(), MAX_TOKEN_X_AMOUNT)
+    amountY = min(tokenY.functions.balanceOf(account.address).call(), MAX_TOKEN_Y_AMOUNT)
     amountMin = 100
 
     print(deltaIds)
